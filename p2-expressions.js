042ed613ffc5fbb170e72e7169adcd71317900ec
keitabacon <100260920+keitabacon@users.noreply.github.com>
2023-06-09 21:34:36 -0700

    Create p2-expressions.js

---
+++ p2-expressions.js
@@ -0,0 +1,37 @@
+/*
+    CIT 281 Project 2
+    Name: Keita Bacon
+*/
+
+// Returns a random number between min (inclusive) and max (exclusive)
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+// get a random number from 1-26 and get it from alphabet and console.log it
+const getRandomLetter = function() {
+    console.log( alphabet[getRandomInteger(1,alphabet.length-1)])
+    return  alphabet[getRandomInteger(1,alphabet.length-1)]
+}
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+let result = "";
+for (let i = 0; i < getRandomInteger(5, 26); i++) {
+    result += alphabet[getRandomInteger(1,alphabet.length-1)];
+    getRandomLetter()
+}
+console.log(result);
+
+
+// console.logs a random collection of 10-20 lower case letters
+const getRandomString = function (minLength, maxLength){
+    let result = "";
+    for (let i = 0; i < getRandomInteger(minLength, maxLength); i++) {
+        result += getRandomLetter()
+    }
+    return result
+}
+console.log(getRandomString(10,21))
+// sorts the string alphabetically
+const getSortedString = function(string){
+return Array.from(string).sort().join('')
+}
+console.log(getSortedString("kshdbkasjdhfbasaaaaa"))
